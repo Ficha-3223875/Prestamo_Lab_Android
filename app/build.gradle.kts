81d1a9eac1f9018383b49a11d55bf6169af67549
Frank junior benitez mosquera <frankjunior5400@gmail.com>
2026-09-15 10:08:02 -0500

    se actualizo 9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
 
     // ⬇️ CORRECCIÓN: Alinea Java y Kotlin a la misma versión JVM
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
@@ -36,7 +36,7 @@ android {
 // ⬇️ CORRECCIÓN: Configura el target de Kotlin fuera del bloque android
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
 
